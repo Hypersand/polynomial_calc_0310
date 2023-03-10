13f3d9c850c041fea802f42276a039800783ec4e
Hypersand <on8214@gmail.com>
2023-03-10 16:24:09 +0900

    다중 덧셈, 뺄셈 기능

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,13 +1,35 @@
 package com.ll;
+
 public class Calc {
 
     public static int run(String exp) {
-        String[] bits = exp.split(" \\+ ");
+
+        boolean needToPlus = exp.contains("+");
+        boolean needToMinus = exp.contains("-");
+
+        String[] bits = null;
+
+        if (needToPlus) {
+            bits = exp.split(" \\+ ");
+        } else if (needToMinus) {
+            bits = exp.split(" \\- ");
+        }
 
         int a = Integer.parseInt(bits[0]);
         int b = Integer.parseInt(bits[1]);
 
-        return a + b;
+        if (needToPlus) {
+            int sum = 0;
+            for (String bit : bits) {
+                sum += Integer.parseInt(bit);
+            }
+            return sum;
+        }
+        if (needToMinus) {
+            return a - b;
+        }
+
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 
 }
